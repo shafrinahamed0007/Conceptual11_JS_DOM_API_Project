6340bdc7707968bb6d0ffb490e4fe897eba0aa1c
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-03-06 01:48:38 +0600

    Design card with html file

--- script/index.js
+++ script/index.js
@@ -4,18 +4,17 @@ async function loadCategories() {
   const res = await fetch(
     "https://openapi.programming-hero.com/api/categories",
   );
+
   const data = await res.json();
-  //   console.log(data);
 
   data.categories.forEach((category) => {
-    // console.log(category.category_name);
+    // console.log(category.category_name)
+
     const btn = document.createElement("button");
     btn.className = "btn bg-outline my-1 w-full";
     btn.textContent = category.category_name;
     categoriesContainer.appendChild(btn);
   });
-
-  
 }
 
 loadCategories();
