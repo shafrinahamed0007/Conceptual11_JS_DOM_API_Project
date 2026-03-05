a48c31d5be0f2d15ed86686d944a40e4915168f6
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-03-06 02:30:26 +0600

    Loading trees data from API

--- script/index.js
+++ script/index.js
@@ -1,5 +1,7 @@
 const categoriesContainer = document.getElementById("categoriesContainer");
+const treesContainer = document.getElementById("treesContainer");
 
+// load All Categoires
 async function loadCategories() {
   const res = await fetch(
     "https://openapi.programming-hero.com/api/categories",
@@ -17,4 +19,43 @@ async function loadCategories() {
   });
 }
 
+
+// loading tress data
+async function loadTress() {
+  const res = await fetch("https://openapi.programming-hero.com/api/plants");
+  const data = await res.json();
+  displayTress(data.plants);
+}
+
+
+// display trees details
+function displayTress(trees) {
+  // console.log(trees)
+  trees.forEach((tree) => {
+    const card = document.createElement("div");
+    card.className = "card bg-base-100  shadow-sm";
+    card.innerHTML = `
+        <figure class="rounded-md m-2 h-[178px] object-cover">
+                            <img src="${tree.image}" title = "${tree.name}" />
+                        </figure>
+                        <div class="card-body">
+                            <h2 class="card-title">${tree.name}</h2>
+                            <p class="text-left text-[12px] line-clamp-2">${tree.description}</p>
+                            <div class="grid grid-cols-2 ">
+                                <div class="badge bg-[#DCFCE7] text-[#15803D] py-1 px-3 ">${tree.category}</div>
+                                <p class="text-right text-[#1F2937]">৳${tree.price}</p>
+
+                            </div>
+                            <div class="card-actions justify-end mt-4">
+
+                                <button class="btn bgPrimary py-3 p-5 w-full rounded-4xl">Add To Cart</button>
+                            </div>
+                        </div>
+        
+        `;
+        treesContainer.appendChild(card)
+  });
+}
+
 loadCategories();
+loadTress();
